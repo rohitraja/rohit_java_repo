895975f1f2307b968605c98316284f59a676d5ab
Rohit Raja <rohit.raja56@gmail.com>
2016-04-16 23:49:39 -0700

    initial commit

---
+++ LearnJavaWithRohit/src/main/java/com/rohit/exceptionHandling/FinallyTest.java
@@ -0,0 +1,18 @@
+package com.rohit.exceptionHandling;
+/*
+ * @author: Rohit
+ * @disc: This is to test finally gets executed 
+ * in any case
+ */
+public class FinallyTest {
+	public static boolean check(){
+		try {
+			return true;
+		} finally {
+			return false;
+		}
+	}
+	public static void main(String[] args){
+		System.out.println(check());
+	}
+}
---
+++ LearnJavaWithRohit/src/main/java/com/rohit/exceptionHandling/TestClass.java
@@ -0,0 +1,8 @@
+package com.rohit.exceptionHandling;
+
+public class TestClass {
+	public static void main(String []args){
+		System.out.println("Rohit");
+	}
+
+}
