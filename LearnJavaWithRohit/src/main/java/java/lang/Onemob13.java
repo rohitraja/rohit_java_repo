21c2b72087d952811932b2167f3a1828cb7b04c4
rohit.raja <rohit.raja56@gmail.com>
2016-06-18 10:04:54 +0530

    commit

---
+++ LearnJavaWithRohit/src/main/java/java/lang/Onemob13.java
@@ -0,0 +1,24 @@
+package java.lang;
+
+
+public final class Onemob13 {
+	
+    class Inner {
+        void test() {
+                if (Onemob13.this.flag); {
+                        sample();
+                }
+        }
+}
+private boolean flag = true;
+public void sample() {
+        System.out.println("Sample");
+}
+public Onemob13() {
+        (new Inner()).test();
+}
+public static void main(String args []) {
+        new Onemob13();
+}
+
+}
